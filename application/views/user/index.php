89fec2733d0f97a94610b2d0464c66de61fdcf2d
Danila004 <danilashel36@gmail.com>
2025-12-12 12:22:21 +0300

    Update index.php for cms

--- application/views/user/index.php
+++ application/views/user/index.php
@@ -3,9 +3,9 @@ use ItForFree\SimpleMVC\Config;
 
 $User = Config::getObject('core.user.class');
 ?>
-<?php include('includes/admin-users-nav.php'); ?>
 
-<h2>Список пользователей</h2> 
+<div id="container">
+<h1>Users</h2> 
     
 <?php if (!empty($users)): ?>
 <table class="table">
@@ -13,8 +13,7 @@ $User = Config::getObject('core.user.class');
     <tr>
       <th scope="col">id</th>
       <th scope="col">Логин</th>
-      <th scope="col">Email</th>
-      <th scope="col">Зарегистрирован</th>
+      <th scope="col">Active</th>
       <th scope="col"></th>
     </tr>
      </thead>
@@ -24,10 +23,9 @@ $User = Config::getObject('core.user.class');
         <td> <?= $user->id ?> </td>
         <td> <?= "<a href=" . \ItForFree\SimpleMVC\Router\WebRouter::link('admin/adminusers/index&id=' 
 		. $user->id . ">{$user->login}</a>" ) ?> </td>
-        <td>  <?= $user->email ?> </td>
-        <td>  <?= $user->timestamp ?> </td>
+        <td>  <?= $user->active ? 'Yes' : 'No' ?> </td>
         <td>  <?= $User->returnIfAllowed("admin/adminusers/edit",
-                    "<a href=" . \ItForFree\SimpleMVC\Router\WebRouter::link("admin/adminusers/edit&id=". $user->id) 
+                    "<a href=" . \ItForFree\SimpleMVC\Router\WebRouter::link("admin/adminusers/editUser&id=". $user->id) 
                     . ">[Редактировать]</a>");?></td>
     </tr>
     <?php endforeach; ?>
@@ -35,6 +33,11 @@ $User = Config::getObject('core.user.class');
     </tbody>
 </table>
 
+<p><?php echo $totalRows?> article<?php echo ( $totalRows != 1 ) ? 's' : '' ?> in total.</p>
+
+<p><a class="nav-link" href="<?=ItForFree\SimpleMVC\Router\WebRouter::link("admin/adminusers/addUser") ?>">Add a new User </a></p>
+
 <?php else:?>
     <p> Список пользователей пуст. </p>
 <?php endif; ?>
+</div>
