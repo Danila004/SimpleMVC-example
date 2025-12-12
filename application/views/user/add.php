fe4122750fdcd2ade279cbf0cd5c7b824b1f531d
Danila004 <danilashel36@gmail.com>
2025-12-12 12:24:49 +0300

    Update add.php for cms

--- application/views/user/add.php
+++ application/views/user/add.php
@@ -1,29 +1,44 @@
+<?php 
+use ItForFree\SimpleMVC\Config;
+
+$Url = Config::getObject('core.router.class');
+$User = Config::getObject('core.user.class');
+?>
+
 <?php include('includes/admin-users-nav.php'); ?>
-<h2><?= $addAdminusersTitle ?></h2>
 
-<form id="addUser" method="post" action="<?= \ItForFree\SimpleMVC\Router\WebRouter::link("admin/adminusers/add")?>"> 
 
-    <div class="form-group">
-        <label for="login">Введите имя пользователя</label>
-        <input type="text" class="form-control" name="login" id="login" placeholder="имя пользователя">
+<form id="addUser" method="post" action="<?= $Url::link("admin/adminusers/addUser")?>">
+    <h1><?php echo htmlspecialchars($results['pageTitle']); ?></h1>
+
+    <input type="hidden" name="userId" value="<?php echo $results['user']->id; ?>"/>
+    
+    <ul>
+        <li>
+            <label for="login">Username</label>
+            <input type="text" name="login" id="login" 
+                placeholder="Username" required 
+                value="<?php echo htmlspecialchars($results['user']->login ?? ''); ?>"/>
+        </li>
+        
+        <li>
+            <label for="password">Password</label>
+            <input type="password" name="password" id="password" 
+                placeholder="Password" required value=<?php $results['user']->pass?> />
+            
+        </li>
+        
+        <li>
+            <label for="active">Active</label>
+            <input type="checkbox" name="active"
+                value="1" <?php if($results['user']->active) echo "checked"; ?>/>
+        </li>
+    </ul>
+        
+    <div class="buttons">
+        <input type="submit" name="saveNewUser" value="Save Changes"/>
+        <input type="submit" formnovalidate name="cancel" value="Cancel"/>
     </div>
-    <div class="form-group">
-        <label for="pass">Введите пароль</label>
-        <input type="text" class="form-control"  name="pass" id="pass" placeholder="пароль">
-    </div>
-    <div class="form-group">   
-        <label for="role">Права доступа</label>
-        <select name="role" id="role" class="form-control"> 
-            <option value="admin">Администратор</option>
-            <option value="auth_user">Зарегистрированный пользователь</option>
-        </select>
-    </div>
-    <div class="form-group">
-        <label for="email">Введите e-mail </label>
-        <input type="text" class="form-control"  name="email" id="email" placeholder="адрес электропочты">
-    </div>
-    <input type="submit" class="btn btn-primary" name="saveNewUser" value="Сохранить">
-    <input type="submit" class="btn" name="cancel" value="Назад">
+
+    
 </form>
-
-
