a5e9f4f19d5f265f12cdf98ae2a2d07902860fe0
Danila004 <danilashel36@gmail.com>
2025-12-12 12:04:40 +0300

    Update AjaxController.php для показа контента статей

--- application/controllers/AjaxController.php
+++ application/controllers/AjaxController.php
@@ -12,6 +12,50 @@ class AjaxController extends \ItForFree\SimpleMVC\MVC\Controller
     public function likeAction()
     {
        echo 'привет!';
-    }   
+    }
+
+    public function getAction()
+    {
+        $article = new Article();
+        $articleId = (int)$_GET['articleId'];
+        $responseArticle = $article->getById($articleId);
+
+        print_r($_GET);        // Проверить параметры
+        
+
+        if ($responseArticle) {
+        echo json_encode([
+            'success' => true,
+            'content' => $responseArticle->content,
+            'method' => 'GET'
+        ], JSON_UNESCAPED_UNICODE);
+        } else {
+            echo json_encode([
+                'success' => false,
+                'error' => 'Статья с ID ' . $articleId . ' не найдена'
+            ]);
+        }
+        
+    }
+
+    public function postAction() 
+    {
+        $jsonInput = file_get_contents('php://input');
+        $input = json_decode($jsonInput, true);
+        $articleId = (int)$input['articleId'];
+
+        // Получаем статью из базы данных
+        $article = new Article();
+        $responseArticle = $article->getById($articleId);
+        
+        if ($responseArticle) {   
+            // Формируем успешный ответ
+            echo json_encode([
+                'success' => true,
+                'content' => $responseArticle->content,
+                'method' => 'POST'
+            ], JSON_UNESCAPED_UNICODE);
+        } 
+    }
 }
 
